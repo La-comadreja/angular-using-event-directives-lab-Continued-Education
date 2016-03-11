e181d0e7b93c09933d4c609d801b27d33ede1a1b
La-comadreja (Rebecca Anne Sealfon) <rebecca@emissary.io>
2016-03-11 01:23:26 -0500

    do the lab with bonus task

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,15 @@
 function VoteController() {
     this.votes = 0;
+    this.originalVotes = this.votes;
 
     this.incrementVotes = function () {
-
+    	if (this.votes <= this.originalVotes)
+    		this.votes++;
     };
 
     this.decrementVotes = function () {
-
+    	if (this.votes >= this.originalVotes)
+    		this.votes--;
     };
 }
 
